1b265918bc3e3d65c93c7024db78b0d31b140a79
Fravic <fravicf@gmail.com>
2013-03-17 22:55:26 -0400

    Button pressed state

--- android/dumbo/src/com/fawna/dumbo/FingerprintShowActivity.java
+++ android/dumbo/src/com/fawna/dumbo/FingerprintShowActivity.java
@@ -2,6 +2,8 @@ package com.fawna.dumbo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 
 public class FingerprintShowActivity extends Activity {
@@ -13,5 +15,7 @@ public class FingerprintShowActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+    Button identifyButton = (Button)findViewById(R.id.button_identify);
+
   }
 }
