5b35e72811d640bbe206842525373aad243da8b1
Fravic <fravicf@gmail.com>
2013-03-17 23:47:14 -0400

    Scale cover photo

--- android/dumbo/src/com/fawna/dumbo/FingerprintShowActivity.java
+++ android/dumbo/src/com/fawna/dumbo/FingerprintShowActivity.java
@@ -1,9 +1,14 @@
 package com.fawna.dumbo;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 
 public class FingerprintShowActivity extends Activity {
@@ -13,9 +18,24 @@ public class FingerprintShowActivity extends Activity {
    */
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-    Button identifyButton = (Button)findViewById(R.id.button_identify);
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
 
+      Display display = getWindowManager().getDefaultDisplay();
+      int screenWidth = display.getWidth();
+
+      // Scale cover correctly
+      ImageView cover = (ImageView)findViewById(R.id.coverphoto);
+      Drawable d = cover.getDrawable();
+      int intendedWidth = screenWidth;
+      int originalWidth = d.getIntrinsicWidth();
+      int originalHeight = d.getIntrinsicHeight();
+      float scale = (float)intendedWidth / originalWidth;
+      int newHeight = Math.round(originalHeight * scale);
+      cover.setLayoutParams(new RelativeLayout.LayoutParams(
+              FrameLayout.LayoutParams.WRAP_CONTENT,
+              FrameLayout.LayoutParams.WRAP_CONTENT));
+      cover.getLayoutParams().width = intendedWidth;
+      cover.getLayoutParams().height = newHeight;
   }
 }
